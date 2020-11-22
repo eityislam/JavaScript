fbbed3a69c01edfbc46773de5d1671fdd0eca883
eity-islam <rimiislameity8@gmail.com>
2020-11-22 16:26:19 +0600

    start learning javascript programming

---
+++ JavaScript-Fundamental/constant.js
@@ -0,0 +1,8 @@
+const total_holuder_gura = 100;
+var make_cook = 100;
+
+var total = total_holuder_gura - make_cook;
+
+total_holuder_gura = 100;
+
+console.log(total)
---
+++ JavaScript-Fundamental/variable.js
@@ -0,0 +1,38 @@
+/*
+What is Variable ?
+Answer: Variable is a data-type that can be store data during program execution.
+*/
+
+// variable decation step;
+
+// 1. Data_type
+// 2. Variable name
+// 3. value
+
+var number = 20;
+var name = "Rimi Islam Eity";
+// Data_type variable_name = value;
+
+// console.log(name);
+// console.log(number);
+
+var total_holuder_gura = 100;
+var make_cook = 17;
+
+console.log(total_holuder_gura)
+
+total_holuder_gura = total_holuder_gura - make_cook;
+
+make_cook = 30;
+console.log(total_holuder_gura)
+total_holuder_gura = total_holuder_gura - make_cook;
+
+make_cook = 53;
+
+console.log(total_holuder_gura)
+
+total_holuder_gura = total_holuder_gura -make_cook;
+
+total_holuder_gura = 500;
+
+console.log(total_holuder_gura);
